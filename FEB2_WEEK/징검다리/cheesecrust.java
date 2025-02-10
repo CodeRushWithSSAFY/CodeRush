0dbbdd14ce7bf61cd9c84a36764fa2f339094ee0
JUNG YEON SU <52970725+cheesecrust@users.noreply.github.com>
2025-02-10 21:47:32 +0900

    Update cheesecrust.java

--- FEB2_WEEK/징검다리/cheesecrust.java
+++ FEB2_WEEK/징검다리/cheesecrust.java
@@ -1,1 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static StringTokenizer st;
+    
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); 
+        st = new StringTokenizer(br.readLine());
+        int[] stoneArr = new int[N + 1];
+        int[] answerArr = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            stoneArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (stoneArr[i] > stoneArr[j]) {
+                    answerArr[i] = Math.max(answerArr[i], answerArr[j] + 1);
+                }                                
+            }
+            answer = Math.max(answer, answerArr[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
 
